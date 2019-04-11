82578c13fea53bac95e18fbb054387c659c57525
Lincoln Patton <lincoln.patton@gmail.com>
2019-04-11 17:48:52 -0400

    Stsrted getting the format of the API layed out

---
+++ GroupMeUp/Controllers/TeamController.cs
@@ -0,0 +1,28 @@
+﻿using GroupMeUp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace GroupMeUp.Controllers
+{
+    [RoutePrefix("api/teams")]
+    public class TeamController : ApiController
+    {
+        [Route("")]
+        [HttpGet]
+        public IEnumerable<Team> GetAllTeams()
+        {
+            return null;
+        }
+
+        [Route("{teamID:int}")]
+        [HttpGet]
+        public Team GetByID(int teamID)
+        {
+            return null;
+        }
+    }
+}
---
+++ GroupMeUp/Controllers/UserController.cs
@@ -0,0 +1,28 @@
+﻿using GroupMeUp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace GroupMeUp.Controllers
+{
+    [RoutePrefix("api/users")]
+    public class UserController : ApiController
+    {
+        [Route("")]
+        [HttpGet]
+        public IEnumerable<User> GetAllUsers()
+        {
+            return null;
+        }
+
+        [Route("{userID:int}")]
+        [HttpGet]
+        public User GetByID(int userID)
+        {
+            return null;
+        }
+    }
+}
