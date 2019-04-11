bf184afbb42b0b6a56876ede1990f6fa9240e34d
Lincoln Patton <lincoln.patton@gmail.com>
2019-04-11 15:04:30 -0400

    Put data classes in the Model folder and changed their namespace. Also tested some custom API calls.

---
+++ GroupMeUp/Models/Message.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupMeUp.Models
+{
+    public struct Message
+    {
+        public string text;
+        public int userID;
+        public DateTime time;
+    }
+}
--- GroupMeUp/Models/Team.cs
+++ GroupMeUp/Models/Team.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Team_Manager
+namespace GroupMeUp.Models
 {
     class Team
     {
