c1a40cfc9aa2de55935dd3b57c14a5497f0e057f
Biggicus-Slutticus <49292456+Biggicus-Slutticus@users.noreply.github.com>
2019-04-25 17:23:21 -0400

    Add files via upload

--- GroupMeUp/DataBaseHandler.cs
+++ GroupMeUp/DataBaseHandler.cs
@@ -1,99 +1,235 @@
 ﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
 using System.Data.SqlClient;
+using System.Text.RegularExpressions;
 using System.Data;
-using GroupMeUp.Models;
-using System.Collections.Generic;
+using System.TimeSpan;
 
 public class DataBaseHandler
 {
-    string conString;
-    SqlConnection con;
-    //Gets the authority level of a user
-    public role getUserAuth(int userID)
+    public DataBaseHandler()
     {
-        
-        //temp var
-        role position = role.USER;
-        char pos;
 
-        if (con.State == ConnectionState.Open)
-        {
-
-            SqlCommand user_auth = new SqlCommand("SELECT role FROM [UserTeam] WHERE userID ='"
-                + userID + "'", con);
-
-            pos = (char)user_auth.ExecuteScalar();
-
-            if (pos.Equals('a'))
-                position = role.ADMIN;
-            else if (pos.Equals('u'))
-                position = role.USER;
-            else if (pos.Equals('o'))
-                position = role.OWNER;
-            else
-                position = role.USER;
-
-        }
-        return position;
     }
 
-    //gets all messages for a certain team
-    public Message[] getMessagesByTeam(int teamID)
-    {
-        string conString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\Big Boss\\Documents\\SowftwareENGR\\Connection\\Connection\\Database1.mdf;Integrated Security=True";
-        SqlConnection con = new SqlConnection(conString);
-        con.Open();
+    //Methods to handle the DB
 
-        //temp var
+    //*******************************************************************************************************************
+    //User
 
-
-        SortedSet<Message> messageSet = new SortedSet<Message>();
-
-        if (con.State == ConnectionState.Open)
+        //Gets the authority level of a user
+        String getUserAuth(int userID)
         {
+            //Get data base
+            string conString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\Big Boss\\Documents\\SowftwareENGR\\Connection\\Connection\\Database1.mdf;Integrated Security=True";
+            SqlConnection con = new SqlConnection(conString);
+            con.Open();
 
-            SqlCommand get_message = new SqlCommand("SELECT body,UseruserID,TeamteamID FROM [Messages] WHERE teamID ='"
-                + teamID + "'", con);
+            //temp var
+            String position;
+            char pos;
 
-            SqlDataReader reader = get_message.ExecuteReader();
-            while (reader.Read())
+            if (con.State == ConnectionState.Open)
             {
-                Message temp = new Message();
-                temp.text = (String)reader.GetValue(1);
-                temp.userID = (int)reader.GetValue(2);
-                temp.teamID = (int)reader.GetValue(3);
-                messageSet.Add(temp);
+
+                SqlCommand user_auth = new SqlCommand("SELECT role FROM [UserTeam] WHERE userID ='"
+                    + userID + "'", con);
+
+                pos = (char)user_auth.ExecuteScalar();
+
+                if (pos.Equals('a'))
+                    position = "admin";
+                else if (pos.Equals('u'))
+                    position = "user";
+                else if (pos.Equals('o'))
+                    position = "owner";
+                else
+                    position = "error";
+                  
+            }
+                return position;
+        }
+
+        //add user
+        void addUser(String uN, String pass, time b, time e)
+        {
+            string conString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\Big Boss\\Documents\\SowftwareENGR\\Connection\\Connection\\Database1.mdf;Integrated Security=True";
+            SqlConnection con = new SqlConnection(conString);
+            con.Open();
+
+            if (con.State == ConnectionState.Open)
+            {
+                String query = "INSERT INTO [User](username,password,beginning, end) VALUES(@username,@password,@beggining, @end)";
+                SqlCommand cmd = new SqlCommand(query, con);
+
+                cmd.Parameters.AddWithValue("@username", uN);
+                cmd.Parameters.AddWithValue("@password", pass);
+                cmd.Parameters.AddWithValue("@beggining", b);
+                cmd.Parameters.AddWithValue("@end", e);
+                cmd.ExecuteNonQuery();
             }
         }
-        Message[] message = new Message[messageSet.Count];
-        return message;
-    }
 
-    public void addMessage(Message message)
-    {
-        string conString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\Big Boss\\Documents\\SowftwareENGR\\Connection\\Connection\\Database1.mdf;Integrated Security=True";
-        SqlConnection con = new SqlConnection(conString);
-        con.Open();
 
-        if (con.State == ConnectionState.Open)
+
+    //*******************************************************************************************************************
+    //Team
+
+        //add team
+        void addTeam(String tN, time b, time e)
         {
-            String query = "INSERT INTO [Messages](teamID,userID,message) VALUES(@teamID,@userID,@message)";
-            SqlCommand cmd = new SqlCommand(query, con);
+            string conString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\Big Boss\\Documents\\SowftwareENGR\\Connection\\Connection\\Database1.mdf;Integrated Security=True";
+            SqlConnection con = new SqlConnection(conString);
+            con.Open();
 
-            cmd.Parameters.AddWithValue("@teamID", message.teamID);
-            cmd.Parameters.AddWithValue("@userID", message.userID);
-            cmd.Parameters.AddWithValue("@message", message.text);
-            cmd.ExecuteNonQuery();
+            if (con.State == ConnectionState.Open)
+            {
+                String query = "INSERT INTO [Team](teamName,beginning, end) VALUES(@teamName,@beggining, @end)";
+                SqlCommand cmd = new SqlCommand(query, con);
+
+                cmd.Parameters.AddWithValue("@teamName", tN);
+                cmd.Parameters.AddWithValue("@beggining", b);
+                cmd.Parameters.AddWithValue("@end", e);
+                cmd.ExecuteNonQuery();
+            }
         }
 
-    }
-    public DataBaseHandler()
-	{
-        //Get data base
-        conString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\Big Boss\\Documents\\SowftwareENGR\\Connection\\Connection\\Database1.mdf;Integrated Security=True";
-        con = new SqlConnection(conString);
-        con.Open();
+        // add user on team
+        void addUserOnTeam(int uID, int tID, String role)
+        {
+            string conString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\Big Boss\\Documents\\SowftwareENGR\\Connection\\Connection\\Database1.mdf;Integrated Security=True";
+            SqlConnection con = new SqlConnection(conString);
+            con.Open();
+
+            if (con.State == ConnectionState.Open)
+            {
+                String query = "INSERT INTO [UserOnTeam](userID,teamID,role) VALUES(@userID,@teamID,@role)";
+                SqlCommand cmd = new SqlCommand(query, con);
+
+                cmd.Parameters.AddWithValue("@userID", uID);
+                cmd.Parameters.AddWithValue("@teamID", tID);
+                cmd.Parameters.AddWithValue("@role", role);
+                cmd.ExecuteNonQuery();
+            }
+        }
 
 
-    }
+
+    //*****************************************************************************************************************
+    //Messages
+
+    //gets all messages for a certain team
+    void getMessage(int teamID)
+        {
+            string conString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\Big Boss\\Documents\\SowftwareENGR\\Connection\\Connection\\Database1.mdf;Integrated Security=True";
+            SqlConnection con = new SqlConnection(conString);
+            con.Open();
+
+            //temp var
+            String message;
+
+            if (con.State == ConnectionState.Open)
+            {
+
+                SqlCommand get_message = new SqlCommand("SELECT body FROM [Messages] WHERE teamID ='"
+                    + teamID + "'", con);
+
+                message = (String)get_message.ExecuteScalar();
+
+                /********************
+                 *  HOWEVER THE FUCK WE PRINT THE SHIT
+                 * *****************/
+            }
+        }
+
+        void addMessage(String mes, int teamID, int userID)
+        {
+            string conString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\Big Boss\\Documents\\SowftwareENGR\\Connection\\Connection\\Database1.mdf;Integrated Security=True";
+            SqlConnection con = new SqlConnection(conString);
+            con.Open();
+
+            if(con.State == ConnectionState.Open)
+            {
+                String query = "INSERT INTO [Messages](teamID,userID,message) VALUES(@teamID,@userID,@message)";
+                SqlCommand cmd = new SqlCommand(query, con);
+
+                cmd.Parameters.AddWithValue("@teamID", teamID);
+                cmd.Parameters.AddWithValue("@userID", userID);
+                cmd.Parameters.AddWithValue("@message", mess);
+                cmd.ExecuteNonQuery();
+            }
+            
+        }
+
+
+    //***************************************************************************************************************
+    //Time
+        
+        //for the meet up times
+        void updateTimeFrame(int teamID)
+        {
+            string conString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\Big Boss\\Documents\\SowftwareENGR\\Connection\\Connection\\Database1.mdf;Integrated Security=True";
+            SqlConnection con = new SqlConnection(conString);
+            con.Open();
+
+            if (con.State == ConnectionState.Open)
+            {
+                time eT;
+                time bT;
+                //begin
+                SqlCommand get_bTime = new SqlCommand("SELECT MAX(beggining) as bTime FROM User, Team, UserOnTeam " +
+                    "WHERE User.userID = UserOnTeam.userID AND UserOnTeam.teamID = Team." + teamID + "", con);
+
+                bT = (time)get_bTime.ExecuteScalar();
+
+                //end
+                SqlCommand get_eTime = new SqlCommand("SELECT MIN(end) as eTime FROM User, Team, UserOnTeam " +
+                    "WHERE User.userID = UserOnTeam.userID AND UserOnTeam.teamID = Team." + teamID + "", con);
+
+                eT = (time)get_eTime.ExecuteScalar();
+
+                //Update
+                String query = "UPDATE Team SET begining = @beginning, end = @end FROM Team WHERE teamID = " + teamID + "";
+                SqlCommand cmd = new SqlCommand(query, con);
+
+                cmd.Parameters.AddWithValue("@begging", bT);
+                cmd.Parameters.AddWithValue("@end", eT);
+                cmd.ExecuteNonQuery();
+
+            }
+        }
+
+        //get a teams beggining meet up time
+        time getBegginingTime(int teamID)
+        {
+            string conString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\Big Boss\\Documents\\SowftwareENGR\\Connection\\Connection\\Database1.mdf;Integrated Security=True";
+            SqlConnection con = new SqlConnection(conString);
+            con.Open();
+
+            if (con.State == ConnectionState.Open)
+            {
+                SqlCommand get_bTime = new SqlCommand("SELECT beggining FROM Team " +
+                    "WHERE teamID = " + teamID + "", con);
+
+               return (time)get_bTime.ExecuteScalar();
+            }
+        }
+
+        //get the end of a teams meet up time
+        time getEndTime(int teamID)
+        {
+            string conString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\Big Boss\\Documents\\SowftwareENGR\\Connection\\Connection\\Database1.mdf;Integrated Security=True";
+            SqlConnection con = new SqlConnection(conString);
+            con.Open();
+
+            if (con.State == ConnectionState.Open)
+            {
+                SqlCommand get_eTime = new SqlCommand("SELECT end FROM Team " +
+                    "WHERE teamID = " + teamID + "", con);
+
+                return (time)get_eTime.ExecuteScalar();
+            }
+        }
 }
